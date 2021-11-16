960a29b96ba36ebf86b5b1aeb1b58c767cbeede7
BrianTorrejon <briantorrejon@hotmail.com>
2021-11-16 14:50:07 -0300

    Se realizaron dos ejercicios más.

--- JavaScript-2/tareas/tareas.js
+++ JavaScript-2/tareas/tareas.js
@@ -150,12 +150,28 @@ function operadoresLogicos(num1, num2, num3) {
   //Si num3 es más grande que num1 y num2, aumentar su valor en 1 y retornar el nuevo valor.
   //0 no es ni positivo ni negativo. Si alguno de los argumentos es 0, retornar "Error".
   //Si no se cumplen ninguna de las condiciones anteriores, retornar false. 
+  if (num1 === 0 || num2 === 0 || num3 === 0) {
+    return "Error";
+  }
+  if (num1 < 0 || num2 < 0 || num3 < 0) {
+    return "Hay negativos";
+  } else if (num1 > 0 && num1 > num2 && num1 > num2) {
+    return "Número 1 es mayor y positivo";
+  }
+  if (num3 > num1 && num3 > num2) {
+    return num3 + 1;
+  }
+  return false;
 }
 
 function esVerdadero(valor) {
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor) {
+    return "Soy verdadero";
+  }
+  return "Soy falso";
 }
 
 function tieneTresDigitos(numero) {
